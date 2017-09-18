84b614f26843ee604218355c123343c5b299f7c6
Luan <luann.rodrigos@gmail.com>
2017-09-18 11:20:45 -0300

    Script de ordenação etet area

--- Script/script.js
+++ Script/script.js
@@ -1,7 +1,13 @@
-function ordenaNumeros(a, b) {
-    return (a - b);
+//função para adicionar itens
+function ler(){
+    var elementos = document.getElementById("itens").value; //get the text from a textarea with id text
+    
+   // var itens = elementos.split(""); // split the text using the space as a delimiter
+
+ 
+	document.getElementById("ler").onclick = ler; //attach a listener to a button with Id read
+ 
+
+console.log(elementos);
+
 }
-
-
-
-console.log(dados.sort(ordenaNumeros));
